c2df0ea69b07b81f30458731780d3b17304524db
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-09 00:35:06 -0500

    Fix linters

--- todos_app/src/components/Header.jsx
+++ todos_app/src/components/Header.jsx
@@ -4,6 +4,7 @@ import styles from "@/styles/Header.module.css";
 
 const Header = () => {
   return (
+    // eslint-disable-next-line
     <header className={styles.header}>
       <h1>To do list</h1>
       
